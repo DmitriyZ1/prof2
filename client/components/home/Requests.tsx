a14c4782a6f4932735bc222ac38fce0062f3f157
dimon <zavorn@mail.ru>
2026-01-25 21:38:24 +0300

    obnova

--- client/components/home/Requests.tsx
+++ client/components/home/Requests.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react"
 import { HOST } from "@/options"
 import Modal from "@/components/different/Modal"
-import parse from 'html-react-parser';
 import Descript from "../different/Descript";
 
 import "./Requests.scss"
@@ -20,20 +19,39 @@ export default function Requests() {
     const [modal, setModal] = useState<boolean>(false)
     const [list, setList] = useState<string[]>([])
 
-    useEffect(() => {   
+    useEffect(() => {
         uploadData()
     }, [])
 
     useEffect(() => {
-        if(!modal){
+        if (!modal) {
             setList([])
         }
     }, [modal])
 
-    async function uploadData(){
+    async function uploadData() {
         const data = await fetch(`${HOST}/decide`)
         const result = await data.json()
         setCards(result)
+    }
+
+    const getRandomInt =(min:number, max:number):number => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min)
+    }
+
+    const ListRandom = (n: number, listArr: string[]) => {
+        const exception:number[] = []
+        const newArr: string[] = []
+        while (newArr.length < n) {
+            const n = getRandomInt(0, listArr.length)
+            if(!exception.includes(n)){
+                exception.push(n)
+                newArr.push(listArr[n])
+            }
+        }
+        return newArr
     }
 
     const showModal = () => {
@@ -53,7 +71,7 @@ export default function Requests() {
                                 </div>
                                 <div className="card__text">
                                     <h3 className="card__tittle">{item.title}
-                                        <span className="card__detailed" onClick={() => {showModal(), setList(item.descrip)}}> Подробнее...</span>
+                                        <span className="card__detailed" onClick={() => { showModal(), setList(ListRandom(7, item.descrip)) }}> Подробнее...</span>
                                     </h3>
                                     {/* <div className="card__other">{item.descripLite}</div> */}
                                 </div>
@@ -62,7 +80,7 @@ export default function Requests() {
                     ))}
                 </ul>
             </div>
-            {modal && <Modal funClose={showModal}> <Descript list={list}/></Modal>}
+            {modal && <Modal funClose={showModal}> <Descript list={list} /></Modal>}
         </div>
     )
 }
